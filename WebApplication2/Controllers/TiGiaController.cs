2ea5fdee209abab3a015be8ad73ece3bc55dea56
NQH1611 <88624093+NQH1611@users.noreply.github.com>
2022-09-19 16:34:54 +0700

    first commit

---
+++ WebApplication2/Controllers/TiGiaController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Xml.Serialization;
+using WebApplication2.Models;
+using System.Dynamic;
+
+namespace WebApplication2.Controllers
+{
+
+    public class TiGiaController : Controller
+    {
+        public IActionResult Index()
+        {
+            string siteContent = string.Empty;
+
+            // link XML của Vietcombank
+            string url = "https://portal.vietcombank.com.vn/Usercontrols/TVPortal.TyGia/pXML.aspx";
+
+            //dùng HTTPWebRequest
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.AutomaticDecompression = DecompressionMethods.GZip;
+            //lấy đối tượng Response
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            //gọi hàm GetResponseStream() để trả về đối tượng Stream
+            Stream responseStream = response.GetResponseStream();
+            //convert từ XML qua C# model:
+            XmlSerializer serializer = new XmlSerializer(typeof(ExrateList));
+            dynamic mymodel = new ExpandoObject();
+            mymodel.Exrate = get
+
+            ExrateList exrateList = (ExrateList)serializer.Deserialize(responseStream);
+            Exrate exrate = (Exrate)serializer.Deserialize(responseStream);
+            return View(exrate);
+            //lấy danh sách Extrates truyền qua cho View
+            return View(exrateList.Exrates);
+        }
+    }
+}
---
+++ WebApplication2/Models/Exrate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace WebApplication2.Models
+{
+    public class Exrate
+    {
+        ExrateList exrateList;
+        [XmlAttribute(AttributeName = "CurrencyCode")]
+        public string CurrencyCode { get; set; }
+        [XmlAttribute(AttributeName = "CurrencyName")]
+        public string CurrencyName { get; set; }
+        [XmlAttribute(AttributeName = "Buy")]
+        public string Buy { get; set; }
+        [XmlAttribute(AttributeName = "Transfer")]
+        public string Transfer { get; set; }
+        [XmlAttribute(AttributeName = "Sell")]
+        public string Sell { get; set; }
+    }
+}
